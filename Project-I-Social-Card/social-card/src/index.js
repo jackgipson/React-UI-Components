32172606b53f247862a458e4ebad2cf2304939e5
jackgipson <jackjgipson@gmail.com>
2019-03-14 10:19:30 -0700

    aligned everything in its correct file.

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -4,38 +4,9 @@ import './index.css';
 import App from './App';
 
 
-import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import HeaderTitle from './components/HeaderComponents/HeaderTitle';
-import HeaderContent from './components/HeaderComponents/HeaderContent';
-import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
 
-import CardContainer from './components/CardComponents/CardContainer';
-import CardBanner from './components/CardComponents/CardBanner';
-import CardContent from './components/CardComponents/CardContent';
 
-import Footer from './components/FooterComponents/Footer';
-
-//import "./index.css";
-
-function Card() {
-    return (
-        <div className='HeaderContainer'>
-        <div className='Lambda-Image'>
-        <ImageThumbnail />
-        <div className='Content'>
-        <HeaderTitle />
-        <HeaderContent />
-        </div>
-        </div>
-        <CardContainer />
-        <CardBanner />
-        <CardContent />
-        <Footer />
-        </div>
-    )
-}
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Card />, rootElement);
+ReactDOM.render(<App />, rootElement);
 
-//ReactDOM.render(<App />, document.getElementById('root'));
