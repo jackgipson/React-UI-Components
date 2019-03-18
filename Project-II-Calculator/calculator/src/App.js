499c7afe9a3162cfbf911df21e36d5ea8d06836f
jackgipson <jackjgipson@gmail.com>
2019-03-18 15:56:05 -0700

    finished calc

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import ButtonContainer from "./components/ButtonComponents/ButtonContainer";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-     <NumberButton />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+
+        <div className="calculator-wrapper"> 
+        <CalculatorDisplay />
+        <ButtonContainer className="clear-button">
+          <ActionButton className="special-button" text="clear" />
+          <NumberButton bgColor="red-button" text="/" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="7" />
+          <NumberButton bgColor="num-button" text="8" />
+          <NumberButton bgColor="num-button" text="9" /> 
+          <NumberButton bgColor="red-button" text="X" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="4" />
+          <NumberButton bgColor="num-button" text="5" />
+          <NumberButton bgColor="num-button" text="6" />
+          <NumberButton bgColor="red-button" text="-" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor="num-button" text="1" />
+          <NumberButton bgColor="num-button" text="2" />
+          <NumberButton bgColor="num-button" text="3" />
+          <NumberButton bgColor="red-button" text="+" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <ActionButton className="special-button" text="0" />
+          <NumberButton bgColor="red-button" text="=" />
+        </ButtonContainer>
+      </div>
+    );
+  }
+}
 
 export default App;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,38 +1,10 @@
 import React from "react";
 import "./Button.css";
 
-const  NumberButton = (props) => {
+const NumberButton = props => {
   return (
-    <div className="NumberButton">
-    <div className='calc-container'>
-      <div className="nums">
-        <button className='clear-button'>Clear</button>
-        <button>/</button>
-      </div>
-      <div className="nums">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
-      </div>
-      <div className="nums">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-      </div>
-      <div className="nums">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-      </div>
-      <div className="nums">
-        <button className='zero-button'>0</button>
-        <button>=</button>
-        </div>
-      </div>
-    </div>
+    <button className={`number-button ${props.bgColor}`}>{props.text}</button> // 'number-button' is added as a class name to style the buttons
+    
   );
 };
 
